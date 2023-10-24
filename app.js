f706119c84e055208e2e8e3da1910b0b7b567811
nurlan-recebov <nurlanrecebov86@gmail.com>
2023-10-24 16:12:05 +0400

    firstcommit

---
+++ app.js
@@ -0,0 +1,19 @@
+const cursor = document.querySelector(".cursor")
+var timeout
+document.addEventListener("mousemove", (e) => {
+    let x = e.pageX
+    let y = e.pageY
+    cursor.style.top = y + "px"
+    cursor.style.left = x + "px"
+    cursor.style.display = 'block'
+
+    function mouseStoped() {
+        cursor.style.display = 'none'
+    }
+    clearTimeout(timeout)
+    timeout = setTimeout(mouseStoped, 1000)
+})
+document.addEventListener("mouseout", () => {
+    cursor.style.display = 'none'
+
+})
